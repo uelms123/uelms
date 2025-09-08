bd22db81dbe8d03fde5e7dbf210a8053d7748987
uelms123 <uelms2025@gmail.com>
2025-09-08 16:24:41 +0530

    Add files via upload

--- routes/unitRoutes.js
+++ routes/unitRoutes.js
@@ -9,19 +9,19 @@ const File = require('../models/files');
 
 // Ensure uploads directory exists
 const uploadsDir = '/var/data/uploads';
-
 fsPromises.mkdir(uploadsDir, { recursive: true }).catch(console.error);
 
 // Multer configuration for file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'Uploads/');
+    cb(null, uploadsDir);  // Use the persistent disk path
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     cb(null, uniqueSuffix + '-' + file.originalname);
   },
 });
+
 
 const upload = multer({
   storage: storage,
