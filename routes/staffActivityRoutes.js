5a9a8367815d3c55fbe01275e898d775bd807e41
uelms123 <uelms2025@gmail.com>
2026-01-22 17:25:18 +0530

    Update staffActivityRoutes.js

--- routes/staffActivityRoutes.js
+++ routes/staffActivityRoutes.js
@@ -20,7 +20,7 @@ const validateObjectId = (req, res, next) => {
 };
 
 router.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'https://uelms.com');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   if (req.method === 'OPTIONS') {
@@ -800,5 +800,6 @@ router.get('/:id', validateObjectId, async (req, res) => {
     });
   }
 });
+
 
 module.exports = router;
